8d557bf67f4e6d8d0c5f7ee075c52a850522f89f
bernazyzz <zebernardomoniz@gmail.com>
2025-06-26 23:05:18 +0100

    majority types of typescript

--- TypeScript/dist/typescript-2.js
+++ TypeScript/dist/typescript-2.js
@@ -15,3 +15,58 @@ let wizard = {
 // null and undefined
 let meh = undefined;
 let noo = null;
+// Tuple
+let basket;
+basket = ["basketball", 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let sizeName = Size[2];
+// Any - !!!!!! BE CAREFUL
+let whatever = "aghhhhhh noooooo!!!";
+whatever = basket;
+// void - when function doesn't return
+let sing = () => {
+    console.log("lalalala");
+};
+// never - when function never return and doesn't have a reachable endpoint
+// usually returns an error
+let error = () => {
+    throw Error("ooops");
+};
+let fightRobotArmy = (robots) => {
+    console.log("FIGHT!");
+};
+let fightRobotArmy2 = (robots) => {
+    console.log("FIGHT!");
+};
+// Function 
+let fightRobotArmyF = (robots) => {
+    console.log("FIGHT!");
+};
+let fightRobotArmy2F = (robots) => {
+    console.log("FIGHT!");
+};
+// Classes
+class Animal {
+    constructor(sound) {
+        this.sing = "lalalala"; // usually is public
+        this.sing = sound;
+    }
+    greet() {
+        return `Hello, ${this.sing}`;
+    }
+}
+let lion = new Animal("RAAAWWWR");
+// lion.greet()
+// Union Type - could be one or the other
+let confused = "hello";
+//In TypeScript, there are several places where type inference
+//is used to provide type information when there is no explicit
+//type annotation. For example, in this code
+let x = 3;
+// automatimally detexts x is a number.
--- TypeScript/typescript-2.ts
+++ TypeScript/typescript-2.ts
@@ -20,3 +20,77 @@ let wizard: object = {
 // null and undefined
 let meh: undefined = undefined;
 let noo: null = null;
+
+// Tuple
+let basket: [string, number];
+basket = ["basketball", 5];
+
+// Enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let sizeName: string = Size[2];
+
+// Any - !!!!!! BE CAREFUL
+let whatever: any = "aghhhhhh noooooo!!!";
+whatever = basket;
+
+// void - when function doesn't return
+let sing = (): void => {
+  console.log("lalalala");
+};
+
+// never - when function never return and doesn't have a reachable endpoint
+// usually returns an error
+let error = (): never => {
+  throw Error("ooops");
+};
+
+// interface
+interface RobotArmy {
+  count: number;
+  type: string;
+  magic?: string;
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+  console.log("FIGHT!");
+};
+let fightRobotArmy2 = (robots: {count: number, type: string, magic?: string}) => {
+  console.log("FIGHT!");
+};
+
+// Function 
+let fightRobotArmyF = (robots: RobotArmy): void => {
+  console.log("FIGHT!");
+};
+let fightRobotArmy2F = (robots: {count: number, type: string, magic?: string}): void => {
+  console.log("FIGHT!");
+};
+
+// Classes
+class Animal {
+  private sing: string = "lalalala"; // usually is public
+  constructor(sound: string) {
+      this.sing = sound;
+  }
+  
+  greet(): string {
+      return `Hello, ${this.sing}`
+  }
+}
+
+let lion = new Animal("RAAAWWWR")
+// lion.greet()
+
+
+// Union Type - could be one or the other
+let confused: string | number = "hello"
+
+//In TypeScript, there are several places where type inference
+//is used to provide type information when there is no explicit
+//type annotation. For example, in this code
+let x = 3;
+// automatimally detexts x is a number.
