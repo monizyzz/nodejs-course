9de17a8433db1ef01a9317934ee4e30dcf83e142
bernazyzz <zebernardomoniz@gmail.com>
2025-06-26 21:59:18 +0100

    ts types

---
+++ TypeScript/dist/typescript-2.js
@@ -0,0 +1,17 @@
+// boolean
+let isCool = true;
+// number
+let age = 56;
+// string
+let eyeColor = "brown";
+let favouriteQuote = `I'm not old, I'm only ${age}`;
+// Array
+let pets = ["cat", "dog", "pig"];
+let pets2 = ["lion", "dragon", "lizard"];
+// Object
+let wizard = {
+    a: "John",
+};
+// null and undefined
+let meh = undefined;
+let noo = null;
---
+++ TypeScript/dist/typescript.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => {
+    return a + b;
+};
+const answer = sum(4, 5);
+console.log(answer);
